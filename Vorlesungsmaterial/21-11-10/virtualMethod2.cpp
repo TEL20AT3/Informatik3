f719632f9c79ee07928bad8782bc751aee62995c
I523036 <tobias.joschko@sap.com>
2021-11-09 21:54:03 +0100

    weiteres Beispiel virtuelle Methoden

---
+++ Vorlesungsmaterial/21-11-10/virtualMethod2.cpp
@@ -0,0 +1,30 @@
+#include "../header.h"
+
+class Base {
+public:
+   void virtual getNameVirtual() {
+      cout << "Base::getNameVirtual()\n";
+   }
+   void getName() {
+      cout << "Base::getName()\n";
+   }
+};
+class Derived: public Base {
+   void virtual getNameVirtual() {
+      cout << "Derived::getNameVirtual()\n";
+   }
+   void getName() {
+      cout << "Derived::getName()\n";
+   }
+};
+
+
+int main(int argc, char const *argv[])
+{
+   Derived d;
+   Base& base_ref = d;
+
+   base_ref.getNameVirtual();
+   base_ref.getName();
+   return 0;
+}
